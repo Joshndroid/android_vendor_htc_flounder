0055fdd57ce8bdcddc3c8addf84a1b466bb3647b
Joshndroid <fmxjoshiewah@gmail.com>
2016-09-21 22:05:19 +1000

    Cleanup : Update to NRD90R source
    Based on AOSP extractor by @anestisb
    Modified for AOSPA by @joshndroid

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,4 +1,13 @@
-# Auto-generated file, do not edit
-
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_PARTITION_SIZE := 268435456
+# Copyright 2016 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
